f6fbab95122fc51e56a7618d8ed0753e87427c67
01fe22bca057 <samuelkotikela14@gmail.com>
2024-02-09 14:46:06 +0530

    program for mathematical functions using java

---
+++ MathematicalOperations.java
@@ -0,0 +1,22 @@
+
+public class MathematicalOperations {
+    public static void main(String[] args) {
+        double absoluteValue = Math.abs(5.67);
+        System.out.println("Absolute Value: " + absoluteValue);
+
+        double squareRoot = Math.sqrt(25);
+        System.out.println("Square Root: " + squareRoot);
+
+        double exponential = Math.exp(2);
+        System.out.println("Exponential: " + exponential);
+
+        int min = Math.min(10, 20);
+        System.out.println("Minimum: " + min);
+
+        int max = Math.max(10, 20);
+        System.out.println("Maximum: " + max);
+
+        double roundedValue = Math.round(5.67);
+        System.out.println("Rounded Value: " + roundedValue);
+    }
+}
